d143fa38e1015a1767e19e4ef6cbf6df366108da
ViniKroth <vinicius.kroth@acad.pucrs.br>
2019-03-22 20:40:20 -0300

    fix: code review changes

--- src/main/java/br/pucrs/ages/mase/user_service/UserServiceApplication.java
+++ src/main/java/br/pucrs/ages/mase/user_service/UserServiceApplication.java
@@ -2,8 +2,10 @@ package br.pucrs.ages.mase.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
-@SpringBootApplication
+@EnableWebFlux
+SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
