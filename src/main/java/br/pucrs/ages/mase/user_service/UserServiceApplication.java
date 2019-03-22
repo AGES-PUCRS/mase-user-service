0fb377aa0768ce8b9c5a75178f50f29f3f1cfb2e
ViniKroth <vinicius.kroth@acad.pucrs.br>
2019-03-22 20:12:04 -0300

    fix project structure

--- src/main/java/br/pucrs/ages/mase/user_service/UserServiceApplication.java
+++ src/main/java/br/pucrs/ages/mase/user_service/UserServiceApplication.java
@@ -1,11 +1,9 @@
-package br.pucrs.mase.userservice;
+package br.pucrs.ages.mase.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
-@EnableWebFlux
-@SpringBootApplication()
+@SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
