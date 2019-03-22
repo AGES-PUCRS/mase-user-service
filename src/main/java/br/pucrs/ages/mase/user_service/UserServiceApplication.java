2aa9208d30acff4417448c6da5b6b98050f3664e
ViniKroth <vinicius.kroth@acad.pucrs.br>
2019-03-22 20:42:00 -0300

    change code review requests

--- src/main/java/br/pucrs/ages/mase/user_service/UserServiceApplication.java
+++ src/main/java/br/pucrs/ages/mase/user_service/UserServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableWebFlux
-SpringBootApplication
+@SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
