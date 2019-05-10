92b3bb663b01686052baa3acf4d6644a87ab79b3
Rafael Ritter <rafaritter44@hotmail.com>
2019-05-10 20:02:50 -0300

    Fix users POST endpoints

--- src/main/java/br/pucrs/ages/mase/user_service/dto/CivilDefenseOfficialDto.java
+++ src/main/java/br/pucrs/ages/mase/user_service/dto/CivilDefenseOfficialDto.java
@@ -2,7 +2,7 @@ package br.pucrs.ages.mase.user_service.dto;
 
 import javax.validation.constraints.NotEmpty;
 
-public class CivilDefenseOfficialDto {
+public class CivilDefenseOfficialDto extends UserDto {
 	
 	private @NotEmpty String office;
 	private @NotEmpty String institutionalLink;
