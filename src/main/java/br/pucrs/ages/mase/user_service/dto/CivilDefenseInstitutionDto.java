622fde5cd58795eb4cfe7cbabf4f35287d108561
ViniKroth <vinicius.kroth@acad.pucrs.br>
2019-05-18 11:58:28 -0300

    test: create civilDefenseInstitutionServiceTest class

--- src/main/java/br/pucrs/ages/mase/user_service/dto/CivilDefenseInstitutionDto.java
+++ src/main/java/br/pucrs/ages/mase/user_service/dto/CivilDefenseInstitutionDto.java
@@ -3,31 +3,36 @@ package br.pucrs.ages.mase.user_service.dto;
 import javax.validation.constraints.NotEmpty;
 
 public class CivilDefenseInstitutionDto {
-	
-	private @NotEmpty String cnpj;
-	private @NotEmpty String name;
-	private @NotEmpty String email;
-	private @NotEmpty String phoneNumber;
-	private @NotEmpty AddressDto address;
-	
-	public String getCnpj() {
-		return cnpj;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	
-	public AddressDto getAddress() {
-		return address;
-	}
-	
+
+    private String id;
+    private @NotEmpty String cnpj;
+    private @NotEmpty String name;
+    private @NotEmpty String email;
+    private @NotEmpty String phoneNumber;
+    private @NotEmpty AddressDto address;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public AddressDto getAddress() {
+        return address;
+    }
+
 }
